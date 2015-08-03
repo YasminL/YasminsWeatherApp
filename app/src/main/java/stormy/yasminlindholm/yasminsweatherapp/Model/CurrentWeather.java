2207ac26f7beaa6db811d6654c563ca23bf2ffcd
YasminL <yasmin.lindholm@ffcg.se>
2015-08-03 17:19:44 +0200

    added getters and setters to Model CurrentWeather

--- app/src/main/java/stormy/yasminlindholm/yasminsweatherapp/Model/CurrentWeather.java
+++ app/src/main/java/stormy/yasminlindholm/yasminsweatherapp/Model/CurrentWeather.java
@@ -4,4 +4,61 @@ package stormy.yasminlindholm.yasminsweatherapp.Model;
  * Created by yasmin.lindholm on 2015-08-03.
  */
 public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipitation;
+    private String mSummary;
+
+    public String getmIcon() {
+        return mIcon;
+    }
+
+    public void setmIcon(String mIcon) {
+        this.mIcon = mIcon;
+    }
+
+    public long getmTime() {
+        return mTime;
+    }
+
+    public void setmTime(long mTime) {
+         this.mTime = mTime;
+    }
+
+    public double getmTemperature() {
+        return mTemperature;
+    }
+
+    public void setmTemperature(double mTemperature) {
+        this.mTemperature = mTemperature;
+    }
+
+    public double mHumidity() {
+        return mHumidity;
+    }
+
+    public void setmHumidity(double mHumidity) {
+        this.mHumidity = mHumidity;
+    }
+
+    public double getmPrecipitation() {
+        return mPrecipitation;
+    }
+
+    public void setmPrecipitation(double mPrecepitation) {
+        this.mPrecipitation = mPrecepitation;
+    }
+
+    public String getmSummary() {
+        return mSummary;
+    }
+
+    public void setmSummary(String mSummary) {
+        this.mSummary = mSummary;
+    }
+
+
+
 }
