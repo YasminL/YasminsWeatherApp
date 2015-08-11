be059744e0aed85822339ee473fd56353005289e
YasminL <yasmin.lindholm@ffcg.se>
2015-08-11 19:08:05 +0200

    when a listItem is pressed in Daily Weather, a Toast is shown with the summary of the weather

--- app/src/main/java/stormy/yasminlindholm/yasminsweatherapp/Controller/DailyForecastActivity.java
+++ app/src/main/java/stormy/yasminlindholm/yasminsweatherapp/Controller/DailyForecastActivity.java
@@ -4,8 +4,12 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
@@ -14,6 +18,8 @@ import stormy.yasminlindholm.yasminsweatherapp.Model.DailyWeather;
 import stormy.yasminlindholm.yasminsweatherapp.R;
 
 public class DailyForecastActivity extends ListActivity {
+
+    public static final String TAG = DailyForecastActivity.class.getSimpleName();
 
     private DailyWeather[] mDailyWeather;
 
@@ -30,6 +36,24 @@ public class DailyForecastActivity extends ListActivity {
         DayAdapter adapter = new DayAdapter(DailyForecastActivity.this, mDailyWeather);
         setListAdapter(adapter);
         }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDailyWeather[position].getDaysOfWeek();
+        Log.i(TAG, "The position is: " + position);
+        String summary = mDailyWeather[position].getSummary();
+        String message;
+
+        if (position == 0) {
+            dayOfTheWeek = "Today";
+            message = String.format("%s the weather will be %s", dayOfTheWeek, summary);
+        } else {
+            message = String.format("On %s the weather will be %s", dayOfTheWeek, summary);
+        }
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
 }
 
 
