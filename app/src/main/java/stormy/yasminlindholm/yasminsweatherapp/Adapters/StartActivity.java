cd0d41090057d30c76b86e2ba6c73f6c4e0077ba
YasminL <yasmin.lindholm@ffcg.se>
2015-08-12 11:02:37 +0200

    added a startpage to write in longitude and latitude

---
+++ app/src/main/java/stormy/yasminlindholm/yasminsweatherapp/Adapters/StartActivity.java
@@ -0,0 +1,19 @@
+package stormy.yasminlindholm.yasminsweatherapp.Adapters;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import stormy.yasminlindholm.yasminsweatherapp.R;
+
+public class StartActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    
+}
