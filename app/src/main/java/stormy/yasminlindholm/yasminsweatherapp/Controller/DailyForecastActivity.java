f3f067af7ce996a458c85cee7d2f96aa8f515e61
YasminL <yasmin.lindholm@ffcg.se>
2015-08-23 23:25:26 +0200

    added screenOrientation to portrait in Manifest

--- app/src/main/java/stormy/yasminlindholm/yasminsweatherapp/Controller/DailyForecastActivity.java
+++ app/src/main/java/stormy/yasminlindholm/yasminsweatherapp/Controller/DailyForecastActivity.java
@@ -2,6 +2,7 @@ package stormy.yasminlindholm.yasminsweatherapp.Controller;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -20,13 +22,19 @@ import stormy.yasminlindholm.yasminsweatherapp.R;
 public class DailyForecastActivity extends ListActivity {
 
     public static final String TAG = DailyForecastActivity.class.getSimpleName();
+    private static final String PREF_NAME = "Location";
+    private static final String PREF_LOCATION = "LocationName";
 
     private DailyWeather[] mDailyWeather;
+    private TextView mlocationLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+
+        mlocationLabel = (TextView) findViewById(R.id.locationLabelWeek);
+        setLocationText();
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
@@ -36,6 +44,12 @@ public class DailyForecastActivity extends ListActivity {
         DayAdapter adapter = new DayAdapter(DailyForecastActivity.this, mDailyWeather);
         setListAdapter(adapter);
         }
+
+    private void setLocationText() {
+        SharedPreferences settings = getSharedPreferences(PREF_NAME, 0);
+        String location = settings.getString(PREF_LOCATION, "empty");
+        mlocationLabel.setText(location);
+    }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
